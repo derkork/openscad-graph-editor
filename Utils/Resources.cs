792ae4fbf6df16e18b80ace429d354f5e64abd2d
Jan Thomä <janthomae@janthomae.de>
2022-04-20 23:41:58 +0200

    chore: make icons with fusion now

--- Utils/Resources.cs
+++ Utils/Resources.cs
@@ -4,9 +4,11 @@ namespace OpenScadGraphEditor.Utils
 {
     public static class Resources
     {
-        public static Texture FunctionIcon => GD.Load<Texture>("res://Icons/function.png");
-        public static Texture VariableIcon => GD.Load<Texture>("res://Icons/variable.png");
-        public static Texture ModuleIcon => GD.Load<Texture>("res://Icons/module.png");
-        public static Texture ScadBuiltinIcon => GD.Load<Texture>("res://Icons/scad_builtin.png");
+        public static Texture FunctionIcon => GD.Load<Texture>("res://Icons/function0000.png");
+        public static Texture VariableIcon => GD.Load<Texture>("res://Icons/variable0000.png");
+        public static Texture ModuleIcon => GD.Load<Texture>("res://Icons/module0000.png");
+        public static Texture ScadBuiltinIcon => GD.Load<Texture>("res://Icons/scad_builtin0000.png");
+        public static Texture ImportIcon => GD.Load<Texture>("res://Icons/import0000.png");
+        
     }
 }
