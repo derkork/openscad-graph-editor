bd365bde0f77ab35b596ee142c7ce2e775fac869
Jan Thomä <janthomae@janthomae.de>
2022-04-26 21:20:47 +0200

    feature: pairwise multiply nodes

---
+++ Nodes/PairwiseMultiply.cs
@@ -0,0 +1,46 @@
+using Godot;
+using JetBrains.Annotations;
+using OpenScadGraphEditor.Library;
+using OpenScadGraphEditor.Utils;
+
+namespace OpenScadGraphEditor.Nodes
+{
+    /// <summary>
+    /// Utility node that does a pairwise multiplication of two arrays.
+    /// </summary>
+    [UsedImplicitly]
+    public class PairwiseMultiply : ScadExpressionNode
+    {
+        public override string NodeTitle => "Pairwise multiply";
+        public override string NodeDescription => "Multiplies the given two vectors pairwise (each element of the first vector is multiplied with the corresponding element of the second vector).";
+
+        public PairwiseMultiply()
+        {
+            InputPorts
+                .Array()
+                .Array();
+
+            OutputPorts
+                .Array();
+        }
+
+        public override string Render(IScadGraph context)
+        {
+            var first = RenderInput(context, 0);
+            var second = RenderInput(context, 1);
+
+            if (first.Empty() || second.Empty())
+            {
+                return "";
+            }
+
+            // because first and second could be expressions, we wrap all of this in a let
+            // block otherwise we go crazy with the parentheses.
+            
+            var var1 = Id.UniqueStableVariableName(0);
+            var var2 = Id.UniqueStableVariableName(1);
+            
+            return $"let({var1} = ({first}), {var2} = ({second})) [for(i =[0:max(len({var1}), len({var2}))-1]) (i < len({var1}) ? {var1}[i] : 0) * (i < len({var2}) ? {var2}[i] : 0)]";
+        }
+    }
+}
---
+++ Nodes/PairwiseMultiplyVector3.cs
@@ -0,0 +1,50 @@
+using Godot;
+using JetBrains.Annotations;
+using OpenScadGraphEditor.Library;
+using OpenScadGraphEditor.Utils;
+
+namespace OpenScadGraphEditor.Nodes
+{
+    /// <summary>
+    /// Utility node that does a pairwise multiplication of two vectors.
+    /// </summary>
+    [UsedImplicitly]
+    public class PairwiseMultiplyVector3 : ScadExpressionNode
+    {
+        public override string NodeTitle => "Pairwise multiply (Vector3)";
+        
+        public override string NodeDescription => "Multiplies the given two vectors pairwise (each element of the first vector is multiplied with the corresponding element of the second vector).";
+
+        
+
+        public PairwiseMultiplyVector3()
+        {
+            InputPorts
+                .Vector3()
+                .Vector3();
+
+            OutputPorts
+                .Vector3(allowLiteral:false);
+        }
+
+        public override string Render(IScadGraph context)
+        {
+            var first = RenderInput(context, 0);
+            var second = RenderInput(context, 1);
+
+            if (first.Empty() || second.Empty())
+            {
+                return "";
+            }
+
+            // because first and second could be expressions, we wrap all of this in a let
+            // block otherwise we go crazy with the parentheses.
+            
+            var var1 = Id.UniqueStableVariableName(0);
+            var var2 = Id.UniqueStableVariableName(1);
+            
+            // we can use a simplified version of the code for the vector3 case
+            return $"let({var1} = ({first}), {var2} = ({second})) [{var1}.x*{var2}.x,{var1}.y*{var2}.y,{var1}.z*{var2}.z]";
+        }
+    }
+}
