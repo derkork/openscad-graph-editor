7a6c65c9e356ef2eb6e45605a802a046b472ab41
Jan Thomä <janthomae@janthomae.de>
2022-10-29 15:18:55 +0200

    feat: add support for all vector2 customizer variants

--- Widgets/NumberSpinEdit.cs
+++ Widgets/NumberSpinEdit.cs
@@ -6,67 +6,25 @@ using OpenScadGraphEditor.Nodes;
 namespace OpenScadGraphEditor.Widgets
 {
     [UsedImplicitly]
-    public class NumberSpinEdit : LiteralWidgetBase<SpinBox, NumberLiteral>
+    public class NumberSpinEdit : NumericRangeLiteralWidget<SpinBox,NumberLiteral>
     {
-        public double? Min
-        {
-            get => _min;
-            set
-            {
-                _min = value;
-                SetupBox();
-            }
-        }
-
-        public double Step
-        {
-            get => _step;
-            set
-            {
-                _step = value;
-                SetupBox();
-            }
-        }
-
-        public double? Max
-        {
-            get => _max;
-            set {
-                _max = value;
-                SetupBox();
-            }
-        }
-
         private SpinBox _spinBox;
-        private double? _min;
-        private double _step = 1;
-        private double? _max;
 
         protected override SpinBox CreateControl()
         {
             _spinBox = new SpinBox();
 
-            SetupBox();
+            SetupRange();
 
             _spinBox.GetLineEdit().Connect("focus_exited")
                 .To(this, nameof(OnFocusExited));
-            _spinBox.Connect("value_changed")
-                .To(this, nameof(OnValueChanged));
 
             return _spinBox;
         }
 
-        private void SetupBox()
+        protected override void SetupRange()
         {
-            if (_spinBox == null)
-            {
-                return;
-            }
-            _spinBox.MinValue = Min ?? 0;
-            _spinBox.AllowLesser = Min == null;
-            _spinBox.MaxValue = Max ?? 0;
-            _spinBox.AllowGreater = Max == null;
-            _spinBox.Step = Step;
+            ApplyRange(_spinBox);
         }
 
 
@@ -80,52 +38,14 @@ namespace OpenScadGraphEditor.Widgets
             }
         }
         
-        private void OnValueChanged(float value)
-        {
-            var clamped = false;
-            // clamp the value if necessary
-            if (Min != null && value < Min)
-            {
-                value = (float) Min;
-                clamped = true;
-            }
-            if (Max != null && value > Max)
-            {
-                value = (float) Max;
-                clamped = true;
-            }
-
-            if (clamped)
-            {
-                // overwrite value
-                _spinBox.Value = value;
-            }
-        }
 
         protected override void ApplyControlValue()
         {
-            // clamp value to min/max
-            var clamped = false;
-            var clampedValue = Literal.Value;
-            if (Max != null && clampedValue > Max)
-            {
-                clampedValue = (double) Max;
-                clamped = true;
-            }
-            else if (Min != null && clampedValue < Min)
-            {
-                clampedValue = (double) Min;
-                clamped = true;
-            }
-
-            _spinBox.Value = clampedValue;
-
             // if we modified the value, emit a change event
-            if (clamped)
+            if (ApplyValue(_spinBox, Literal.Value, out var clampedValue))
             {
                 EmitValueChange(new NumberLiteral(clampedValue));
             }
-
         }
     }
 }
