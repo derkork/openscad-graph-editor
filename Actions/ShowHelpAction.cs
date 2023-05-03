38f7443f39ee4471c8b9acb9d1ad78c7f97d4c9e
Jan Thomä <janthomae@janthomae.de>
2023-05-03 22:57:07 +0200

    feat: add help to the context menu of nodes
    fixes #46

---
+++ Actions/ShowHelpAction.cs
@@ -0,0 +1,24 @@
+﻿using JetBrains.Annotations;
+using OpenScadGraphEditor.Widgets;
+
+namespace OpenScadGraphEditor.Actions
+{
+    [UsedImplicitly]
+    public class ShowHelpAction : IEditorAction
+    {
+        public int Order => 99999;
+        public string Group => "";
+        public bool TryBuildQuickAction(IEditorContext context, RequestContext item, out QuickAction result)
+        {
+            if (item.TryGetNode(out var graph, out var node))
+            {
+                result = new QuickAction($"Show help",
+                    () => context.ShowHelp(graph, node));
+                return true;
+            }
+
+            result = default;
+            return false;
+        }
+    }
+}
