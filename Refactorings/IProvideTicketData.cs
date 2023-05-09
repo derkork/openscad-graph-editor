e4bfc54164054eb76e1187bf47af820e86765972
Jan Thomä <janthomae@janthomae.de>
2023-05-09 09:23:45 +0200

    feat: add refactoring tickets

---
+++ Refactorings/IProvideTicketData.cs
@@ -0,0 +1,10 @@
+﻿namespace OpenScadGraphEditor.Refactorings
+{
+    /// <summary>
+    /// Interface for refactorings that provide ticket data.
+    /// </summary>
+    public interface IProvideTicketData<T>
+    {
+        public string Ticket { get; }
+    }
+}
