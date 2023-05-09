5c04fa3c69f4e5f755876dad5abaca0db6440e93
Jan Thomä <janthomae@janthomae.de>
2023-05-09 09:24:41 +0200

    refactor: move copy/paste nodes into a refactoring

---
+++ Refactorings/PasteNodesRefactoring.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Godot;
+using OpenScadGraphEditor.Library;
+using OpenScadGraphEditor.Nodes;
+using OpenScadGraphEditor.Utils;
+
+namespace OpenScadGraphEditor.Refactorings
+{
+    /// <summary>
+    /// Pastes all nodes from the source buffer into the target graph at the given position. If pasteCopy is true,
+    /// the source buffer will be duplicated before pasting, so that the original source buffer is not modified.
+    /// Only nodes that are allowed in the target graph will be pasted.
+    /// Provides the pasted nodes as ticket data.
+    /// </summary>
+    public class PasteNodesRefactoring : Refactoring, IProvideTicketData<List<ScadNode>>
+    {
+        private readonly ScadGraph _targetGraph;
+        private readonly ScadGraph _sourceBuffer;
+        private readonly Vector2 _position;
+        private readonly bool _pasteCopy;
+
+        public string Ticket { get; } = Guid.NewGuid().ToString();
+
+        public PasteNodesRefactoring(ScadGraph targetGraph, ScadGraph sourceBuffer, Vector2 position, bool pasteCopy = true)
+        {
+            _targetGraph = targetGraph;
+            _sourceBuffer = sourceBuffer;
+            _position = position;
+            _pasteCopy = pasteCopy;
+        }
+        
+        public override void PerformRefactoring(RefactoringContext context)
+        {
+            var copy = _pasteCopy 
+	            ? _sourceBuffer.CloneSelection(context.Project, _sourceBuffer.GetAllNodes(), out _) 
+	            : _sourceBuffer;
+            
+            // now the buffer may contain nodes that are not allowed in the given target graph. So we need
+			// to filter these out here and also delete all connections to these nodes.
+			var disallowedNodes = copy.GetAllNodes()
+				.Where(it => !_targetGraph.Description.CanUse(it))
+				.ToList();
+
+			// delete all connections to the disallowed nodes
+			copy.GetAllConnections()
+				.Where(it => disallowedNodes.Any(it.InvolvesNode))
+				.ToList() // avoid concurrent modification
+				.ForAll(copy.RemoveConnection);
+
+			// and the nodes themselves
+			disallowedNodes.ForAll(copy.RemoveNode);
+
+			var scadNodes = copy.GetAllNodes().ToList();
+			if (scadNodes.Count == 0)
+			{
+				return;
+			}
+
+			// we now need to normalize the position of the nodes so they are pasted in the correct position
+			// we do this by finding the bounding box of the nodes and then offsetting them by the difference between the position
+			// of the bounding box and the position of the node that is closes to top left
+
+			// we start with a rectangle that is a point that simply has the position of the first node
+			var boundingBox = new Rect2(scadNodes[0].Offset, Vector2.Zero);
+			// now expand this rectangle so it contains all the points of all the offsets of the nodes
+			boundingBox = scadNodes.Aggregate(boundingBox, (current, node) => current.Expand(node.Offset));
+			// now we calculate the offset of the bounding box position and the desired position
+			var offset = _position - boundingBox.Position;
+
+			// and offset every node by this
+			scadNodes.ForAll(it => it.Offset += offset);
+
+			// build the refactorings to add the given nodes and connections
+			var refactorings = new List<Refactoring>();
+			foreach (var node in scadNodes)
+			{
+				refactorings.Add(new AddNodeRefactoring(_targetGraph, node));
+			}
+
+			foreach (var connection in copy.GetAllConnections())
+			{
+				refactorings.Add(
+					new AddConnectionRefactoring(
+						new ScadConnection(_targetGraph, connection.From, connection.FromPort, connection.To,
+							connection.ToPort
+						)
+					)
+				);
+			}
+			
+			// and finally perform all the refactorings
+			refactorings.ForAll(context.PerformRefactoring);
+			
+			context.StoreTicketData(this, scadNodes);
+        }
+    }
+}
--- Refactorings/RefactoringExt.cs
+++ Refactorings/RefactoringExt.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using Godot;
 using OpenScadGraphEditor.Library;
 using OpenScadGraphEditor.Nodes;
 
@@ -42,6 +43,117 @@ namespace OpenScadGraphEditor.Refactorings
             }
         }
         
+        /// <summary>
+        /// Generates a safe name for any member of the project. If the name is already used, a number is appended to the name.
+        /// </summary>
+        public static string SafeName(this ScadProject project, string name)
+        {
+            if (!project.IsNameUsed(name))
+            {
+                return name;
+            }
+            
+            var result = name;
+            var i = 2;
+            while (project.IsNameUsed(result))
+            {
+                result = $"{name}{i}";
+                i++;
+            }
+
+            return result;
+        }
+
+
+        /// <summary>
+        /// Generates a safe name for a parameter of an InvokableDescription. If the name is already used, a number is appended to the name.
+        /// </summary>
+        public static string SafeParameterName(this InvokableDescription invokableDescription, string name)
+        {
+            if (invokableDescription.Parameters.All(it => it.Name != name))
+            {
+                return name;
+            }
+            
+            var result = name;
+            var i = 2;
+            while (invokableDescription.Parameters.Any(it => it.Name == result))
+            {
+                result = $"{name}{i}";
+                i++;
+            }
+            
+            return result;
+        }
+        
+        /// <summary>
+        /// Creates a new ScadGraph by cloning the selected nodes and the connections between them. Will not clone
+        /// entry and return nodes (e.g. all nodes that cannot be deleted). Returns a dictionary which maps the original
+        /// node ids to the new node ids (useful for post processing).
+        /// </summary>
+        public static ScadGraph CloneSelection(this ScadGraph source,IReferenceResolver referenceResolver, IEnumerable<ScadNode> selection,
+            out Dictionary<string, string> mappedIds)
+        {
+            var result = new ScadGraph();
+            
+            // when we make a copy we need to take special care about bound nodes
+            // each bound node needs to have its partner within the copy even if it was not originally selected.
+            // so we look up all bound nodes in the selection and if its partner is not in the
+            // selection we silently add it.
+
+            var correctedSelection = new HashSet<ScadNode>(selection);
+
+            var boundNodes = correctedSelection.Where(it => it is IAmBoundToOtherNode).ToList();
+            foreach (var boundNode in boundNodes)
+            {
+                var boundTo = (IAmBoundToOtherNode) boundNode;
+                var partner = source.ById(boundTo.OtherNodeId);
+
+                if (!correctedSelection.Contains(partner))
+                {
+                    correctedSelection.Add(partner);
+                }
+            }
+            
+            // remove all nodes which cannot be deleted (which are usually nodes that are built in so they cannot be copied either).
+            var sanitizedSet = correctedSelection
+                .Where(it => !(it is ICannotBeDeleted))
+                .ToHashSet();
+
+            var idMapping = new Dictionary<string, string>();
+            // make copies of all the nodes and put them into the copy buffer
+            foreach (var node in sanitizedSet)
+            {
+                var copy = NodeFactory.Duplicate(node, referenceResolver);
+                // make note of the id mapping, because we need this to resolve connections
+                // and bound nodes later.
+                idMapping[node.Id] = copy.Id;
+                result.AddNode(copy);
+            }
+
+            // correct the id mappings of any bound nodes
+            foreach (var node in result.GetAllNodes().Where(it => it is IAmBoundToOtherNode))
+            {
+                var partnerId = ((IAmBoundToOtherNode) node).OtherNodeId;
+                // find out which id the partner has in the copy
+                var partnerCopyId = idMapping[partnerId];
+                // and set the partner id to the copy id
+                ((IAmBoundToOtherNode) node).OtherNodeId = partnerCopyId;
+            }
+
+            //  copy all connections which are between the selected nodes
+            foreach (var connection in source.GetAllConnections())
+            {
+                if (sanitizedSet.Contains(connection.From) && sanitizedSet.Contains(connection.To))
+                {
+                    result.AddConnection(idMapping[connection.From.Id], connection.FromPort,
+                        idMapping[connection.To.Id], connection.ToPort);
+                }
+            }
+
+            mappedIds = idMapping;
+            return result;
+        }
 
         public readonly struct ReferencingNode<T, TGraphType> where TGraphType : ScadGraph
         {
