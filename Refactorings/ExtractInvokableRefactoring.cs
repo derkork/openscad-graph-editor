836a4af3cd5ab129c7faeb2a09bf5cf7627b3ddd
Jan Thomä <janthomae@janthomae.de>
2023-05-09 21:06:11 +0200

    feat: add refactoring for extracting an invokable from a selection
    fixes #51

--- Refactorings/ExtractInvokableRefactoring.cs
+++ Refactorings/ExtractInvokableRefactoring.cs
@@ -25,7 +25,8 @@ namespace OpenScadGraphEditor.Refactorings
         public override void PerformRefactoring(RefactoringContext context)
         {
             // PHASE 1: Analysis
-            
+            // ====================================================================================================
+
             var clones = _sourceGraph.CloneSelection(context.Project, _selection, out var newIds);
 
             // now we need to check if the selection contains any nodes which are not an expression.
@@ -38,6 +39,9 @@ namespace OpenScadGraphEditor.Refactorings
             var incomingConnections = _sourceGraph.GetAllConnections()
                 // we use the id mapping here as this has the sanitized selection
                 .Where(it => newIds.ContainsKey(it.To.Id) && !newIds.ContainsKey(it.From.Id))
+                // sort them by vertical position to avoid crossing lines later
+                .OrderBy(it => it.From.Offset.y)
+                .ThenBy(it => it.From.Offset.x)
                 .ToList();
 
             // to determine return values we need to find all connections that are connecting from inside the selection
@@ -65,20 +69,21 @@ namespace OpenScadGraphEditor.Refactorings
                     // - all geometry nodes in the selection are connected to either a node outside the selection or
                     //   to the output of the selection (since we have outgoing connections we can ignore the case
                     //   where there are no geometry nodes at all or all geometry nodes are disconnected)
-                    
+
                     var geometryNodes = _selection.Where(it => !(it is IAmAnExpression)).ToList();
                     // check at at least one connection exists where the geometry node is the source
                     if (geometryNodes.Any(it => _sourceGraph.GetAllConnections().All(con => con.From != it)))
                     {
-                        NotificationService.ShowNotification("Cannot extract module. The selection contains some geometry which is not connected to anything and some that is.");
+                        NotificationService.ShowNotification(
+                            "Cannot extract module. The selection contains some geometry which is not connected to anything and some that is.");
                         return;
                     }
-                    
+
                     // now we also need to check that for all outgoing connections one of the following is true:
                     // - all outgoing connections go to the same node outside the selection
                     // - all outgoing connections originate from the same node inside the selection
                     // - all originating nodes inside the selection are connected to all outgoing nodes outside the selection
-                    
+
                     var sources = outgoingConnections.Select(it => it.From).Distinct().ToList();
                     var destinations = outgoingConnections.Select(it => it.To).Distinct().ToList();
 
@@ -86,7 +91,8 @@ namespace OpenScadGraphEditor.Refactorings
                         // assuming we have no duplicate connections
                         outgoingConnections.Count != sources.Count * destinations.Count)
                     {
-                        NotificationService.ShowNotification("Cannot extract module. The selection contains multiple geometry outputs that would get lost when extracting the module.");
+                        NotificationService.ShowNotification(
+                            "Cannot extract module. The selection contains multiple geometry outputs that would get lost when extracting the module.");
                         return;
                     }
                 }
@@ -111,13 +117,12 @@ namespace OpenScadGraphEditor.Refactorings
             }
 
             // PHASE 2: Create the invokable and its graph
-            
+            // ====================================================================================================
+
             // now build an invokable description from the data we have collected
             InvokableDescription invokableDescription;
             if (isModule)
             {
-
-
                 var builder = ModuleBuilder.NewModule(context.Project.SafeName("new_module"));
 
                 // now we create all incoming parameters by using the incoming connections
@@ -155,7 +160,8 @@ namespace OpenScadGraphEditor.Refactorings
             var newGraph = context.Project.AddInvokable(invokableDescription);
 
             // PHASE 3: Populate the graph
-            
+            // ====================================================================================================
+
             // find the entry point
             var entryPoint = newGraph.GetAllNodes().First(it => it is EntryPoint) as EntryPoint;
 
@@ -196,12 +202,12 @@ namespace OpenScadGraphEditor.Refactorings
                     // check how many sources and destinations we have
                     var sources = geometryInputs.Select(it => it.From).Distinct().ToList();
                     var destinations = geometryInputs.Select(it => it.To).Distinct().ToList();
-                    
+
                     // we can use a single children() node if any of these three conditions is true:
                     // - we have only a single source
                     // - we have only a single destination
                     // - all sources are connected to all destinations
-                    
+
                     var useSingleChildrenNode = sources.Count == 1 || destinations.Count == 1 ||
                                                 // assuming there are no duplicate connections
                                                 sources.Count * destinations.Count == geometryInputs.Count;
@@ -209,18 +215,22 @@ namespace OpenScadGraphEditor.Refactorings
                     if (useSingleChildrenNode)
                     {
                         var childrenNode = NodeFactory.Build<Children>();
-                        
+
                         // add the node to the graph
                         context.PerformRefactoring(new AddNodeRefactoring(newGraph, childrenNode));
 
                         // position it below the entry point
                         context.PerformRefactoring(new ChangeNodePositionRefactoring(newGraph, childrenNode,
                             entryPoint.Offset + new Vector2(0, 200)));
-                        
+
+                        // enable children for this graph
+                        context.PerformRefactoring(
+                            new EnableChildrenRefactoring((ModuleDescription) invokableDescription));
+
                         // and connect it's output to all destinations
                         foreach (var connection in geometryInputs)
                         {
-                            var newConnection = new ScadConnection(newGraph, childrenNode, 0, 
+                            var newConnection = new ScadConnection(newGraph, childrenNode, 0,
                                 newGraph.ById(newIds[connection.To.Id]), connection.ToPort);
                             context.PerformRefactoring(new AddConnectionRefactoring(newConnection));
                         }
@@ -229,68 +239,160 @@ namespace OpenScadGraphEditor.Refactorings
                     {
                         // it's a bit more tricky here, we need to create a child(x) node for each incoming connection
                         // and connect it to the corresponding destination.
-                        // first sort the connection by the position of the source node, so we know which child is
-                        // first and last.
-                        var sortedGeometryInputs = geometryInputs
-                            .OrderBy(it => it.From.Offset.y)
-                            .ThenBy(it => it.From.Offset.x)
-                            .ToList();
 
-                        for (var index = 0; index < sortedGeometryInputs.Count; index++)
+                        for (var index = 0; index < geometryInputs.Count; index++)
                         {
-                            var connection = sortedGeometryInputs[index];
-                            
+                            var connection = geometryInputs[index];
+
                             // make a new child node
                             var childNode = NodeFactory.Build<Child>();
 
                             // add the node to the graph
                             context.PerformRefactoring(new AddNodeRefactoring(newGraph, childNode));
                             // position it below the entry point, 200 units down below the previous child
-                            context.PerformRefactoring(new ChangeNodePositionRefactoring(newGraph, childNode, 
+                            context.PerformRefactoring(new ChangeNodePositionRefactoring(newGraph, childNode,
                                 entryPoint.Offset + new Vector2(0, 200 + index * 200)));
-                            
+                            // enable children for this graph
+                            context.PerformRefactoring(
+                                new EnableChildrenRefactoring((ModuleDescription) invokableDescription));
+
                             // and connect it's output to the corresponding destination
-                            var newConnection = new ScadConnection(newGraph, childNode, 0, 
+                            var newConnection = new ScadConnection(newGraph, childNode, 0,
                                 newGraph.ById(newIds[connection.To.Id]), connection.ToPort);
-                            
+
                             context.PerformRefactoring(new AddConnectionRefactoring(newConnection));
-                            
+
                             // also set the input port literal of the child node to the index, so first node
                             // gets 0, second node gets 1, etc.
-                            context.PerformRefactoring(new SetLiteralValueRefactoring(newGraph, childNode, PortId.Input(0), new NumberLiteral(index)));
+                            context.PerformRefactoring(new SetLiteralValueRefactoring(newGraph, childNode,
+                                PortId.Input(0), new NumberLiteral(index)));
                         }
                     }
-                    
                 }
             }
             else
             {
                 // for functions, we need to connect the function result to the return value
                 var functionReturn = newGraph.GetAllNodes().First(it => it is FunctionReturn) as FunctionReturn;
-                
+
                 // determine the node which is most to the right
                 var rightMostNode = newGraph.GetAllNodes()
                     .Where(it => it != functionReturn)
                     .OrderByDescending(it => it.Offset.x)
                     .First();
-                
+
                 // move the function return so it is to the right of the right-most node
-                context.PerformRefactoring(new ChangeNodePositionRefactoring(newGraph, functionReturn, 
+                context.PerformRefactoring(new ChangeNodePositionRefactoring(newGraph, functionReturn,
                     rightMostNode.Offset + new Vector2(200, 0)));
-                
+
                 // now if we have an outgoing connection, find the matching new node and connect it to the function return
                 if (outgoingConnections.Count == 1)
                 {
                     var connection = outgoingConnections.First();
-                    var newConnection = new ScadConnection(newGraph, newGraph.ById(newIds[connection.From.Id]), connection.FromPort, 
+                    var newConnection = new ScadConnection(newGraph, newGraph.ById(newIds[connection.From.Id]),
+                        connection.FromPort,
                         functionReturn, 0);
                     context.PerformRefactoring(new AddConnectionRefactoring(newConnection));
                 }
-                // TODO: find if we have exactly one unconnected expression output port and connect it to the function return
+                else
+                {
+                    // check if we have a single expression node which does not have its output connected. in that 
+                    // case use this as the return value
+                    var candidates = newGraph.GetAllNodes()
+                        .Where(it => it is IAmAnExpression)
+                        .ToList();
+
+                    var allConnections = newGraph.GetAllConnections();
+
+                    // find all nodes which are not the source of a connection
+                    var unconnectedNodes = candidates
+                        .Where(it => allConnections.All(c => c.From != it))
+                        .ToList();
+
+                    // if we have exactly one, use that
+                    if (unconnectedNodes.Count == 1)
+                    {
+                        var connection = new ScadConnection(newGraph, unconnectedNodes[0], 0, functionReturn, 0);
+                        context.PerformRefactoring(new AddConnectionRefactoring(connection));
+                    }
+                }
+            }
+
+            // Phase 4: remove the original nodes and connections and replace them with a call to the new invokable
+            // ====================================================================================================
+            
+            // it is enough to delete the nodes, the refactoring will kill all connections automatically
+            foreach (var node in _selection)
+            {
+                context.PerformRefactoring(new DeleteNodeRefactoring(_sourceGraph, node));
+            }
+
+            // put the new function / module roughly at the center of all the nodes we just deleted
+            var center = _selection.Select(it => it.Offset).Aggregate((a, b) => a + b) / _selection.Count;
+
+            var newInvocation = isModule
+                ? NodeFactory.Build<ModuleInvocation>(invokableDescription)
+                : NodeFactory.Build<FunctionInvocation>(invokableDescription);
+
+            context.PerformRefactoring(new AddNodeRefactoring(_sourceGraph, newInvocation));
+            context.PerformRefactoring(new ChangeNodePositionRefactoring(_sourceGraph, newInvocation, center));
+
+            // now connect the parameters, we need to check however if the module supports children() or not
+            // because that changes the offset of the parameters
+            var hasChildren = isModule && ((ModuleDescription) invokableDescription).SupportsChildren;
+            var parameterOffset = hasChildren ? 1 : 0;
+
+            // only connect expression inputs
+            var parameterConnections = incomingConnections
+                .Where(it => it.TryGetFromPortType(out var type) && type.IsExpressionType())
+                .ToList();
+
+            for (var i = 0; i < parameterConnections.Count; i++)
+            {
+                var connection = parameterConnections[i];
+                var newConnection = new ScadConnection(_sourceGraph, connection.From, connection.FromPort,
+                    newInvocation,
+                    i + parameterOffset);
+                context.PerformRefactoring(new AddConnectionRefactoring(newConnection));
+            }
+
+            // connect all geometry inputs to the geometry input port 
+            var geometryConnections = incomingConnections
+                .Where(it => it.TryGetToPortType(out var type) && type == PortType.Geometry)
+                .ToList();
+
+            foreach (var connection in geometryConnections)
+            {
+                var newConnection = new ScadConnection(_sourceGraph, connection.From, connection.FromPort,
+                    newInvocation,
+                    0); // first port is the geometry input
+                context.PerformRefactoring(new AddConnectionRefactoring(newConnection));
+            }
+
+            // connect geometry outputs
+            if (isModule)
+            {
+                foreach (var connection in outgoingConnections)
+                {
+                    var newConnection =
+                        new ScadConnection(_sourceGraph, newInvocation, 0, connection.To, connection.ToPort);
+                    context.PerformRefactoring(new AddConnectionRefactoring(newConnection));
+                }
+            }
+            else
+            {
+                // if we have a single output connection, connect the return value to the node
+                if (outgoingConnections.Count == 1)
+                {
+                    var newConnection = new ScadConnection(_sourceGraph, newInvocation, 0, outgoingConnections[0].To,
+                        outgoingConnections[0].ToPort);
+                    context.PerformRefactoring(new AddConnectionRefactoring(newConnection));
+                }
             }
         }
 
-        private static List<(string Name, PortType Type)> BuildParameters(IEnumerable<ScadConnection> incomingConnections)
+        private static List<(string Name, PortType Type)> BuildParameters(
+            IEnumerable<ScadConnection> incomingConnections)
         {
             return incomingConnections
                 .Select(it =>
